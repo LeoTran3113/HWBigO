b71fc81a8c4c6453e3e358a72da4cc4a829883c8
KietTran3113 <130996476+LeoTran3113@users.noreply.github.com>
2023-05-05 22:24:46 +1000

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,28 @@
+package cm4structure;
+
+public class Fibonacci {
+	public static void main(String[] args) {
+		
+		int number = 15;
+		findFibonacci(number);
+		
+		System.out.println("Fibonacci number of " + number + " is " + findFibonacci(number));
+		System.out.println("-----------------------");
+		System.out.println("Time Complexity is O(n)");
+	}
+
+	public static int findFibonacci(int num) {
+		int pre = 0;
+		int current = 1;
+		int sum = 0;
+		while (num > 1) {
+			sum = pre + current;
+			pre = current;
+			current = sum;
+			num--;
+		}
+		return sum;
+
+	}
+
+}
