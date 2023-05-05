e9f0ebe5d56133e12b15b730292a38d16ff865db
KietTran3113 <130996476+LeoTran3113@users.noreply.github.com>
2023-05-06 00:32:23 +1000

    Updated Code

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,10 +2,10 @@ package cm4structure;
 
 public class Fibonacci {
 	public static void main(String[] args) {
-		
+
 		int number = 15;
 		findFibonacci(number);
-		
+
 		System.out.println("Fibonacci number of " + number + " is " + findFibonacci(number));
 		System.out.println("-----------------------");
 		System.out.println("Time Complexity is O(n)");
@@ -15,6 +15,13 @@ public class Fibonacci {
 		int pre = 0;
 		int current = 1;
 		int sum = 0;
+
+		if (num == 0) {
+			return 0;
+		}
+		if (num == 1) {
+			return 1;
+		}
 		while (num > 1) {
 			sum = pre + current;
 			pre = current;
